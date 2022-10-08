66d1bd741be96410c48f2b0c1b2b0c5e42566aa7
toan1231 <chauthai2003@gmail.com>
2022-10-07 23:45:08 -0400

    hellowq

--- src/Tuet.java
+++ src/Tuet.java
@@ -2,4 +2,7 @@ public class Tuet {
     public static void main(String[] args) {
        // downloadthechange
     }
+    public static void main1(String[] args) {
+        // downloadthechange
+    }
 }
